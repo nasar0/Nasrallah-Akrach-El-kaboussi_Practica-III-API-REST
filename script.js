bd4c2a1af187745952c4ec8699911891ed8ec9e8
nasrallah <nasaroanas6@gmail.com>
2024-12-03 22:28:29 +0100

    2024-12-03

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 "use strict";
+//variables creadas para el ejercicio
 let selectCategoria = document.querySelector("#categoria select");
 const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
 let row = document.querySelector("#recetas .row");
+let botonAñadir = document.querySelector("#botonAñadir")
+let fav = JSON.parse(localStorage.getItem("dato")) || [];
+let alerta=  document.querySelector("#alert");
+let Cerrar = document.querySelector("#Cerrar");
+let alertap = document.querySelector("#alert #text");
+
 function obtenerDatos(url){
     return fetch(url)
     .then(respuestas => respuestas.json())
@@ -36,29 +43,58 @@ function mostrarRec(cat) {
     })
 }
 function abrirModal(id) {
+
     const url3 ="https://www.themealdb.com/api/json/v1/1/lookup.php?i="+id;
     let titulo = document.querySelector("#emergenteLabel"); 
     let imagenes = document.querySelector("#imagenes"); 
     let instru = document.querySelector("#instru"); 
     let Ingredientes = document.querySelector("#Ingredientes");
     let li = document.createElement("li");
+    Ingredientes.innerHTML=""
     obtenerDatos(url3)
     .then(dato=>{
-            titulo.textContent=dato.meals[0].strMeal;
-            imagenes.src=dato.meals[0].strMealThumb;
-            instru.textContent=dato.meals[0].strInstructions
-            for (let i = 1; i < 21; i++) {
-                let ingre=dato.meals[0][`strIngredient`+i];
-                let cant = dato.meals[0][`strMeasure`+i];
-                if (ingre != "" && ingre != null ) {
-                    let li = document.createElement("li");
-                    li.textContent=ingre+" - "+cant;
-                    Ingredientes.append(li);
-                }
-               
+        titulo.textContent=dato.meals[0].strMeal;
+        imagenes.src=dato.meals[0].strMealThumb;
+        instru.textContent=dato.meals[0].strInstructions
+        for (let i = 1; i < 21; i++) {
+            let ingre=dato.meals[0][`strIngredient`+i];
+            let cant = dato.meals[0][`strMeasure`+i];
+            if (ingre != "" && ingre != null ) {
+                let li = document.createElement("li");
+                li.textContent=ingre+" - "+cant;
+                Ingredientes.append(li);
+            }   
+        }
+        botonAñadir.addEventListener("click", () => {
+            if (!fav.some(f => f.meals[0].idMeal === dato.meals[0].idMeal)) {
+                fav.push(dato); 
+                localStorage.setItem("dato", JSON.stringify(fav));
+                botonAñadir.textContent = "Eliminar de favoritos"; 
+                alerta.style.opacity = 1; 
+                alerta.style.display = "block"; 
+                alertap.textContent="Agregado Correctamente"
+
+            } else {
+                fav = fav.filter(f => f.meals[0].idMeal !== dato.meals[0].idMeal); 
+                localStorage.setItem("dato", JSON.stringify(fav));
+                botonAñadir.textContent = "Añadir a favoritos"; 
+                alertap.textContent="Se a eliminado de favoritos "
             }
+            setTimeout(() => {
+                alerta.style.opacity = 0; 
+                setTimeout(() => {
+                    alerta.style.display = "none"; 
+                }, 1000); 
+            }, 1500);
             
+        });
+        
     })
     
 }
+Cerrar.addEventListener("click", () =>{
+    alerta.style.display = "none";
+})
+
+
 cargarCategorias()
